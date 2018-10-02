d8c045f65a391dfd31e989fd2cc9ddabf11c3839
loljoho <loljoho@users.noreply.github.com>
2018-10-02 19:36:02 -0400

    Implement API fetch() in DriverListContainer

--- src/DriverListContainer.js
+++ src/DriverListContainer.js
@@ -4,11 +4,37 @@ import DriverList from './DriverList';
 class DriverListContainer extends Component {
   constructor() {
     super()
-    const drivers = [2, 4, 6, 8, 1, 3, 5, 7, 9]
-    this.state = { drivers }
+    this.state = {
+      drivers: [],
+      data: [],
+      driverTable: []
+    }
+  }
+  componentDidMount() {
+    // https://developers.google.com/web/updates/2015/03/introduction-to-fetch
+    fetch('http://ergast.com/api/f1/current/drivers.json')
+      .then((response) => {
+        if (response.status !== 200) {
+          console.log('Error status code: ' + response.status);
+          return;
+        }
+        response.json().then((response) => {
+          this.setState({
+            data: response.MRData,
+            driverTable: response.MRData.DriverTable
+          });
+          return response.MRData;
+        });
+      }).catch((err) => {
+        console.log('Error: ' + err);
+      });
+    //this.setState({drivers: [2, 4, 6, 8, 1, 3, 5, 7, 9]})
   }
   render() {
-    return <DriverList drivers={this.state.drivers} />
+    return <DriverList
+              drivers={this.state.drivers}
+              data={this.state.data}
+            />
   }
 }
 
