5c7c6b089801d6489daf097c83de22f19bffb2c8
loljoho <loljoho@users.noreply.github.com>
2018-10-31 15:30:50 -0400

    Comment out logo import for now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { HashRouter, Route, Switch } from "react-router-dom";
 
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 // Layout
