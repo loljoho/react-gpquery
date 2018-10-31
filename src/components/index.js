6e4742e370dfb69dc1ba6fa202d87b400b661f1b
loljoho <loljoho@users.noreply.github.com>
2018-10-31 14:29:26 -0400

    Create Navbar Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
+import Navbar from './Navbar/Navbar';
 import ConstructorTableContainer from './Constructor/ConstructorTableContainer';
 import DriverTableContainer from './Driver/DriverTableContainer';
 import RaceTableContainer from './Race/RaceTableContainer';
 
 export {
+  Navbar,
   ConstructorTableContainer,
   DriverTableContainer,
   RaceTableContainer
