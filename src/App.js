49887be86b30bf92a4af2254a7368c6c5ddefd81
loljoho <loljoho@users.noreply.github.com>
2018-10-31 14:38:11 -0400

    Create Sidebar Component and update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import './App.css';
 
 // Layout Components
 import {
-  Navbar
+  Navbar,
+  Sidebar
 } from './components';
 
 // Components
@@ -23,12 +24,17 @@ class App extends Component {
         <div>
           <Navbar />
           <div className="container-fluid">
-            <Switch>
-              <Route exact path="/" component={RaceTableContainer} />
-              <Route path="/races" component={RaceTableContainer} />
-              <Route path="/drivers" component={DriverTableContainer} />
-              <Route path="/constructors" component={ConstructorTableContainer} />
-            </Switch>
+            <div class="row">
+              <Sidebar />
+              <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+                <Switch>
+                  <Route exact path="/" component={RaceTableContainer} />
+                  <Route path="/races" component={RaceTableContainer} />
+                  <Route path="/drivers" component={DriverTableContainer} />
+                  <Route path="/constructors" component={ConstructorTableContainer} />
+                </Switch>
+              </main>
+            </div>
           </div>
         </div>
       </HashRouter>
