e6ee4996168c82d118f54254bbb42f7eabd45b0d
loljoho <loljoho@users.noreply.github.com>
2018-11-08 12:01:14 -0500

    Create OverviewPage component and set as default route

---
+++ src/pages/OverviewPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import RaceTableContainer from '../components/Race/RaceTableContainer';
+
+class OverviewPage extends Component {
+  render() {
+    return <RaceTableContainer />
+  }
+
+}
+
+export default OverviewPage;
