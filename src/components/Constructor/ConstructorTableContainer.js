8cbe23e4c88cdd24c687fd8ce9a66499d18fb65f
loljoho <loljoho@users.noreply.github.com>
2018-11-12 11:02:50 -0500

    Implement ReactTable for Constructor

--- src/components/Constructor/ConstructorTableContainer.js
+++ src/components/Constructor/ConstructorTableContainer.js
@@ -1,50 +1,93 @@
 import React, { Component } from 'react';
-import ConstructorTable from './ConstructorTable';
+
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+
+const requestData = () => {
+  return fetch('https://ergast.com/api/f1/current/constructorStandings.json')
+    .then(res => {
+      if (res.status !== 200) {
+        console.log('Error status code: ' + res.status);
+        return;
+      }
+      return res.json();
+    })
+    .then(data => {
+      const rows = data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings.map(team => {
+        let row = {};
+
+        row.position        = team.position;
+        row.positionText    = team.positionText;
+        row.points          = team.points;
+        row.wins            = team.wins;
+
+        row.teamId          = team.Constructor.constructorId;
+        row.teamName        = team.Constructor.name;
+        row.teamNationality = team.Constructor.nationality;
+
+        return row;
+      }); // end map
+      const res = {};
+      res.rows = rows;
+      return res;
+    }); // end then
+}
 
 class ConstructorTableContainer extends Component {
   constructor() {
     super()
     this.state = {
-      constructors: [],
-      constructorList: [],
-      data: []
+      data: [],
+      pages: null,
+      loading: true
     }
+    this.fetchData = this.fetchData.bind(this);
   }
-  componentDidMount() {
-    // https://developers.google.com/web/updates/2015/03/introduction-to-fetch
-    fetch('https://ergast.com/api/f1/current/constructorStandings.json')
-      .then((response) => {
-        if (response.status !== 200) {
-          console.log('Error status code: ' + response.status);
-          return;
-        }
-        response.json().then((response) => {
-          console.log('Success!', response);
-          return response.MRData;
-        })
-        .then((data) => {
-          const constructors = data.StandingsTable.StandingsLists[0].ConstructorStandings.map((constructor) => {
-            return {
-              position: constructor.position,
-              positionText: constructor.positionText,
-              points: constructor.points,
-              wins: constructor.wins,
-              constructorId: constructor.Constructor.constructorId,
-              constructor: constructor.Constructor.name,
-              constructorNationality: constructor.Constructor.nationality,
-              url: constructor.Constructor.url
-            };
-          })
-          this.setState({constructors: constructors});
-        });
-      }).catch((err) => {
-        console.log('Error: ' + err);
+  fetchData(state, instance) {
+    this.setState({ loading: true });
+    requestData().then(res => {
+      this.setState({
+        data: res.rows,
+        loading: false
       });
+    });
   }
   render() {
-    return <ConstructorTable
-              constructors={this.state.constructors}
-            />
+    const { data, loading } = this.state;
+    return <ReactTable
+      data={data}
+      columns={[
+        {
+          Header    : 'Pos',
+          id        : 'position',
+          accessor  : 'positionText',
+          maxWidth  : 50,
+        },
+        {
+          Header    : 'Constructor',
+          id        : 'teamId',
+          accessor  : 'teamName'
+        },
+        {
+          Header    : 'Nationality',
+          accessor  : 'teamNationality',
+        },
+        {
+          Header    : 'Wins',
+          accessor  : 'wins',
+          maxWidth  : 50,
+        },
+        {
+          Header    : 'Pts',
+          accessor  : 'points',
+          maxWidth  : 50,
+        },
+      ]}
+      loading={loading}
+      showPagination={false}
+      onFetchData={this.fetchData}
+      className="-highlight"
+    />
   }
 }
 
