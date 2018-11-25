340b72fb6d95c85362fd867d6d045aeee0aef408
loljoho <loljoho@users.noreply.github.com>
2018-11-25 16:47:26 -0500

    Tweak DriverStatCard design

--- src/components/Driver/DriverStatCard.js
+++ src/components/Driver/DriverStatCard.js
@@ -15,14 +15,13 @@ class DriverStatCard extends Component {
     return (
       <Col xs="6" sm="4" md="3" lg="2">
         <Card body className="mb-4">
-          <CardSubtitle></CardSubtitle>
-          <CardTitle>
-            <span>{this.props.value}</span>
+          <CardTitle className="mb-0 text-center">
+            {this.props.value}
             <small className="float-right">
               <FontAwesomeIcon icon={`${this.props.icon}`} />
             </small>
           </CardTitle>
-          <CardText className="text-right"><small>{this.props.name}</small></CardText>
+          <CardText className="text-center"><small>{this.props.name}</small></CardText>
         </Card>
       </Col>
     );
